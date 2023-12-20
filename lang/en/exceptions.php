8fa664d8927c5603fb8d02da0bfe172722c4007a
Luiz Moura <moura.oliveira.luiz@gmail.com>
2023-12-19 21:19:37 -0400

    fix: create error message for anime service request failure

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -3,5 +3,10 @@
 return [
     'storage' => [
         'failed_to_fetch_image' => 'Failed to fetch image content.'
-    ]
+    ],
+    'api' => [
+        'jikan' => [
+            'request_failed' => 'A request failed with the JIKAN api.',
+        ]
+    ],
 ];
