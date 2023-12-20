d815100ad88b746f1c10618c45ecb0e1be034098
Luiz Moura <moura.oliveira.luiz@gmail.com>
2023-12-19 20:48:46 -0400

    feat: create methods for the abstract dto and remove dependency on spatie/laravel-data

---
+++ src/Infra/Abstracts/Contracts/DataTransferObject.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Infra\Abstracts\Contracts;
+
+interface DataTransferObject
+{
+    public static function fromArray(array $data): self;
+    public function toArray(): array;
+}
--- src/Infra/Abstracts/DataTransferObject.php
+++ src/Infra/Abstracts/DataTransferObject.php
@@ -2,8 +2,51 @@
 
 namespace Infra\Abstracts;
 
-use Spatie\LaravelData\Data;
+use Infra\Abstracts\Contracts\Collection;
+use Infra\Abstracts\Contracts\DataTransferObject as DataTransferObjectContract;
+use ReflectionClass;
 
-abstract class DataTransferObject extends Data
+abstract class DataTransferObject implements DataTransferObjectContract
 {
+    public function __construct(array $data = [])
+    {
+        return self::__construct(...$data);
+    }
+
+    public static function fromArray(array $data): static
+    {
+        return new static(...$data);
+    }
+
+    public function toArray(): array
+    {
+        $properties = get_object_vars($this);
+
+        return array_map(function ($property) {
+            return $this->parse($property);
+        }, $properties);
+    }
+
+    private function parse(mixed $property): mixed
+    {
+        if (is_object($property)) {
+            $isEnum = (new ReflectionClass($property::class))->isEnum();
+            if ($isEnum) {
+                return $property->value;
+            }
+
+            $arrayable = method_exists($property, 'toArray');
+            if ($arrayable) {
+                if ($property instanceof Collection) {
+                    $property = $property->toArray();
+
+                    return array_map(fn ($item) => $item->toArray(), $property);
+                }
+
+                return $property->toArray();
+            }
+        }
+
+        return $property;
+    }
 }
