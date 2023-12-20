69cb4ecffde956a718268b61cc894b3101778a23
Luiz Moura <moura.oliveira.luiz@gmail.com>
2023-12-19 21:02:59 -0400

    feat: create repository service provider

---
+++ src/App/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Domain\Animes\Contracts\AnimeRepository as AnimeRepositoryContract;
+use Domain\Animes\Contracts\AnimeTitleRepository as AnimeTitleRepositoryContract;
+use Domain\Animes\Contracts\BroadcastRepository as BroadcastRepositoryContract;
+use Domain\Animes\Contracts\GenreRepository as GenreRepositoryContract;
+use Domain\Animes\Contracts\MemberRepository as MemberRepositoryContract;
+use Domain\Shared\Medias\Contracts\MediaRepository as MediaRepositoryContract;
+use Illuminate\Support\ServiceProvider;
+use Infra\Persistente\Eloquent\Repositories\AnimeRepository;
+use Infra\Persistente\Eloquent\Repositories\AnimeTitleRepository;
+use Infra\Persistente\Eloquent\Repositories\BroadcastRepository;
+use Infra\Persistente\Eloquent\Repositories\GenreRepository;
+use Infra\Persistente\Eloquent\Repositories\MediaRepository;
+use Infra\Persistente\Eloquent\Repositories\UserRepository;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(MemberRepositoryContract::class, UserRepository::class);
+        $this->app->bind(AnimeRepositoryContract::class, AnimeRepository::class);
+        $this->app->bind(AnimeTitleRepositoryContract::class, AnimeTitleRepository::class);
+        $this->app->bind(GenreRepositoryContract::class, GenreRepository::class);
+        $this->app->bind(BroadcastRepositoryContract::class, BroadcastRepository::class);
+        $this->app->bind(MediaRepositoryContract::class, MediaRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot() {}
+}
